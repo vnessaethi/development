3be7fb40a541daca5903f8c1a1da4b474152ad38
Vanessa Santos Fernandes <vnessaethi@gmail.com>
2019-05-16 22:10:57 -0300

    Criando a primeira classe com atributos de um cachorro

--- helloWorld.py
+++ helloWorld.py
@@ -6,4 +6,18 @@ app = Flask(__name__)
 def helloWorld():
     return "Hello World!"
 
-app.run()
+class mamiferos():
+    def __init__(self, name, dono):
+        self.name = name
+        self.dono = dono
+    
+    def latir(self):
+        print('{0} está latindo: Au! Au! Au! Au! Au!'.format(self.name))
+
+zeze = mamiferos('zeze', 'Vanessa')
+
+print('O nome do cachorro é:', zeze.name, 'O dono(a) dele é:', zeze.dono)
+
+zeze.latir()
+
+app.run()   
